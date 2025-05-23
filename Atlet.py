8cb0a0e36ca0ec3bd52ab4a07d6f37dec5080c56
MasWahid05 <syaifudinwahid05@gmail.com>
2025-05-23 09:46:32 +0700

    Update Atlet.py

--- Atlet.py
+++ Atlet.py
@@ -16,7 +16,7 @@ if os.path.exists(logo_path):
     with col2:  # Menggunakan kolom tengah
         st.image(logo_path, width=310)  # Menggunakan st.image untuk menampilkan logo
         st.markdown(
-            "<h3 style='text-align: center;'>Aplikasi Absensi Atlet JMS JUARA</h3>",
+            "<h4 style='text-align: center;'>Aplikasi Absensi Atlet JMS JUARA</h4>",
             unsafe_allow_html=True
         )
 else:
